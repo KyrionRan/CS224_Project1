f8013b5fe9269ba8420cff2c552173567ac92314
KyrionRan <kyrionran@outlook.com>
2026-03-24 11:11:16 -0400

    Update Algorithm Class

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,3 +1,26 @@
-public class Algorithm {
+import java.util.ArrayList;
 
+public class Algorithm {
+    public ArrayList<Applicant> selectionSort(ArrayList<Applicant> arr) {
+        for (int i = 0; i < arr.size() - 1; i++) {
+            int max = i;
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr.get(j).getScore() > arr.get(max).getScore()) {
+                    if (arr.get(j).getId().compareTo(arr.get(max).getId()) < 0) {
+                        if (arr.get(j).getLast_name().compareTo(arr.get(max).getLast_name()) < 0) {
+                            if (arr.get(j).getFirst_name().compareTo(arr.get(max).getFirst_name()) < 0) {
+                                max = j;
+                            }
+                        }
+                    }
+                }
+            }
+            if (i != max) {
+                Applicant temp = new Applicant(arr.get(i));
+                arr.get(i).copy(arr.get(max));
+                arr.get(max).copy(temp);
+            }
+        }
+        return arr;
+    }
 }
