ea01fbaaafde1b8636b4a6e0c2dc974e43ef816a
KyrionRan <kyrionran@outlook.com>
2026-03-24 10:32:39 -0400

    Update Applicant Class

--- src/Applicant.java
+++ src/Applicant.java
@@ -1,2 +1,29 @@
 public class Applicant {
+    private String id;
+    private String first_name;
+    private String last_name;
+    private String score;
+
+    public Applicant(String id, String first_name, String last_name, String score) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.score = score;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getFirst_name() {
+        return this.first_name;
+    }
+
+    public String getLast_name() {
+        return this.last_name;
+    }
+
+    public String getScore() {
+        return this.score;
+    }
 }
