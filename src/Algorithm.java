d577910c40ba8fa36b2548f937c2078d08a492c5
KyrionRan <kyrionran@outlook.com>
2026-03-24 20:01:36 -0400

    Update Algorithm Class

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -6,8 +6,14 @@ public class Algorithm {
             int max = i;
             for (int j = i + 1; j < arr.size(); j++) {
                 if (arr.get(j).getScore() > arr.get(max).getScore()) {
+                    max = j;
+                } else if (arr.get(j).getScore() == arr.get(max).getScore()) {
                     if (arr.get(j).getId().compareTo(arr.get(max).getId()) < 0) {
+                        max = j;
+                    } else if (arr.get(j).getId().compareTo(arr.get(max).getId()) == 0) {
                         if (arr.get(j).getLast_name().compareTo(arr.get(max).getLast_name()) < 0) {
+                            max = j;
+                        } else if (arr.get(j).getLast_name().compareTo(arr.get(max).getLast_name()) == 0) {
                             if (arr.get(j).getFirst_name().compareTo(arr.get(max).getFirst_name()) < 0) {
                                 max = j;
                             }
